6ffab85ca49b4f5b3400e7492d42bf1a34da4ba9
Chris Russell <cdr@encapsule.io>
2015-12-07 11:59:52 -0800

    Test the binplaced arc_graph library.

--- TESTS/core/graph/module-under-test.js
+++ TESTS/core/graph/module-under-test.js
@@ -1,3 +1,3 @@
 // Module base dir is relative to the test-module-resolver script location.
 // Returns a function that accepts submodule name returns submodule exports.
-module.exports = require('../../test-module-resolver')('../SOURCES/core/graph/');
+module.exports = require('../../test-module-resolver')('../BUILD/ARC_core/');
