1dcb17c3c1df942c998cfc3de2b9825def515439
Chris Russell <cdr@encapsule.io>
2016-01-08 13:07:34 -0800

    Stash handlebars hacking... ... going to restructure this because it isn't going quickly enough.

--- SOURCES/tools/arc_tools_lib_filter_doc_gen.js
+++ SOURCES/tools/arc_tools_lib_filter_doc_gen.js
@@ -4,6 +4,112 @@ FILTERLIB = ARCCORE.filter;
 TYPELIB = ARCCORE.types;
 HANDLEBARS  = require('handlebars');
 
+
+var filterlibResponse = FILTERLIB.create({
+    operationID: "Unymh9rRTVaBHGah531gmQ",
+    operationName: "Filter Documentation Generator",
+    operationDescription: "Generates human-readable documentation from a Filter instance.",
+
+    inputFilterSpec: {
+        ____label: "Filter Doc Generator Request",
+        ____description: "Input Filter and options request object.",
+        ____types: "jsObject",
+        filter: {
+            ____label: "Input Filter Instance",
+            ____description: "A reference to the Filter object to document.",
+            ____types: "jsObject",
+            filterDescriptor: {
+                ____label: "Filter Runtime Parameters",
+                ____description: "Internal representation of the filter's runtime contraints.",
+                ____accept: "jsObject"
+            }
+        },
+        context: {
+            ____label: "Document Context Descriptor",
+            ____description: "Required and optional data to pass through to the handlebars template engine.",
+            ____accept: "jsObject",
+            ____defaultValue: {}
+        },
+        template: {
+            ____label: "Handlebars Template",
+            ____description: "Optional handlebar template to override default generator behaviors.",
+            ____types: "jsString",
+            ____defaultValue: "# {{filterDescriptor.operationName}}\n\n" +
+                "Operation: **{{filterDescriptor.operationID}}**<br>\n" +
+                "Input type: **{{inputSignature}}**<br>\n" +
+                "Output type: **{{outputSignature}}**\n\n" +
+                "## Description\n\n" +
+                "{{filterDescriptor.operationDescription}}\n\n" +
+                "## Request Input\n\n" +
+                "Operation {{filterDescriptor.operationID}} is invoked via filter object method `request`.\n\n" +
+                "### Input filter spec {{inputSignature}} JSON:\n\n" +
+                "```JavaScript\n" +
+                "{{{inputJSON}}}\n" +
+                "```\n\n" +
+                "## Response Output\n\n" +
+                "All filters return a normalized response object with the following pseudo-object format:\n\n" +
+                "```{ error: string | null, result: variant }```\n\n" +
+                "Iff response.error is null then response.result is valid. Otherwise, response.error is a string explaining what went wrong.\n\n" +
+                "Iff response.error is not null (i.e. an error occurred), then response.result may contain information that's useful for fault diagnosis.\n\n" +
+                "If no error occurred the format of response.result is governed by this filter's output filter specification.\n\n" +
+                "### Output filter spec {{outputSignature}} JSON:\n\n" +
+                "```JavaScript\n" +
+                "{{{outputJSON}}}\n" +
+                "```\n\n" +
+                "<hr>\n\n" +
+                "Generated with {{generator}}\n"
+        }
+    },
+
+    bodyFunction: function(request_) {
+        var response = { error: null, result: null };
+        var errors = [];
+        var inBreakScope = false;
+        while (!inBreakScope) {
+            inBreakScope = true;
+            try {
+                var compiledTemplate = HANDLEBARS.compile(request_.template);
+                var templateContext = {};
+                templateContext.filterDescriptor = request_.filter.filterDescriptor;
+                templateContext.filterClassification = getFilterClassification(request_.filter.filterDescriptor).result;
+                templateContext.generator =
+                    "[Encapsule/arctools](https://github.com/Encapsule/arctools/) " +
+                    "v" + ARCCORE.__meta.version + " at " +
+                    new Date().toString();
+                templateContext.inputJSON = JSON.stringify(request_.filter.filterDescriptor.inputFilterSpec, undefined, 4);
+                templateContext.inputSignature = ARCCORE.identifier.irut.fromReference(request_.filter.filterDescriptor.inputFilterSpec).result;
+                templateContext.outputJSON = JSON.stringify(request_.filter.filterDescriptor.outputFilterSpec, undefined, 4);
+                templateContext.outputSignature = ARCCORE.identifier.irut.fromReference(request_.filter.filterDescriptor.outputFilterSpec).result;
+
+                var document = compiledTemplate(templateContext);
+                response.result = document;
+            } catch (error_) {
+                errors.unshift(error_.toString());
+                errors.unshift("Fatal exception while attempting generate document:");
+            }
+            break;
+        }
+        if (errors.length) {
+            response.error = errors.join(" ");
+        }
+        return response;
+    },
+
+    outputFilterSpec: {
+        ____label: "Filter Documentation",
+        ____description: "Human-readable Filter function documentation",
+        ____accept: "jsString"
+    }
+
+
+});
+
+if (filterlibResponse.error) {
+    throw new Error(filterlibResponse.error);
+};
+
+module.exports = filterlibResponse.result;
+
 var filterClassificationTable = {
     "jsUndefined:jsUndefined:jsUndefined": "passthrough (NOOP)",
     "jsUndefined:jsUndefined:jsObject":    "response normalizer",
@@ -38,90 +144,3 @@ var getFilterClassification = function(filterDescriptor_) {
     }
     return { error: null, result: classification };
 };
-
-var filterlibResponse = FILTERLIB.create({
-    operationID: "Unymh9rRTVaBHGah531gmQ",
-    operationName: "Filter Documentation Generator",
-    operationDescription: "Generates human-readable documentation from a Filter instance.",
-
-    inputFilterSpec: {
-        ____label: "Filter Doc Generator Request",
-        ____description: "Input Filter and options request object.",
-        ____types: "jsObject",
-        filter: {
-            ____label: "Input Filter Instance",
-            ____description: "A reference to the Filter object to document.",
-            ____types: "jsObject",
-            filterDescriptor: {
-                ____label: "Filter Runtime Parameters",
-                ____description: "Internal representation of the filter's runtime contraints.",
-                ____accept: "jsObject"
-            }
-        },
-        template: {
-            ____label: "Handlebars Template",
-            ____description: "Optional handlebar template to override default generator behaviors.",
-            ____types: "jsString",
-            ____defaultValue: "# {{filterDescriptor.operationName}}\n\n" +
-                "**operationID:** {{filterDescriptor.operationID}} / **classification:** {{filterClassification}}\n\n" +
-                "## Summary\n\n" +
-                "{{filterDescriptor.operationDescription}}\n\n" +
-                "## Operation\n\n" +
-                "A programatically-generated summary of the filter's behavior.\n\n" +
-                "## Signature\n\n" +
-                "### Request\n\n" +
-                "```JavaScript\n" +
-                "{{{inputSummary}}}\n" +
-                "```\n\n" +
-                "### Response\n\n" +
-                "```JavaScript\n" +
-                "{{{outputSummary}}}\n" +
-                "```\n\n" +
-                "<hr>\n\n" +
-                "Generated with {{generator}}\n"
-        }
-    },
-
-    bodyFunction: function(request_) {
-        var response = { error: null, result: null };
-        var errors = [];
-        var inBreakScope = false;
-        while (!inBreakScope) {
-            inBreakScope = true;
-            try {
-                var compiledTemplate = HANDLEBARS.compile(request_.template);
-                var templateContext = {};
-                templateContext.filterDescriptor = request_.filter.filterDescriptor;
-                templateContext.filterClassification = getFilterClassification(request_.filter.filterDescriptor).result;
-                templateContext.generator = "Encapsule/arctools v" + ARCCORE.__meta.version;
-                templateContext.inputSummary = JSON.stringify(request_.filter.filterDescriptor.inputFilterSpec, undefined, 4);
-                templateContext.outputSummary = JSON.stringify(request_.filter.filterDescriptor.outputFilterSpec, undefined, 4);
-
-                var document = compiledTemplate(templateContext);
-                response.result = document;
-            } catch (error_) {
-                errors.unshift(error_.toString());
-                errors.unshift("Fatal exception while attempting generate document:");
-            }
-            break;
-        }
-        if (errors.length) {
-            response.error = errors.join(" ");
-        }
-        return response;
-    },
-
-    outputFilterSpec: {
-        ____label: "Filter Documentation",
-        ____description: "Human-readable Filter function documentation",
-        ____accept: "jsString"
-    }
-
-
-});
-
-if (filterlibResponse.error) {
-    throw new Error(filterlibResponse.error);
-};
-
-module.exports = filterlibResponse.result;
