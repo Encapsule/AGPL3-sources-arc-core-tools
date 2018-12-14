ac188dc577522656507feea3fe9e848a635c78e3
Chris Russell <cdr@encapsule.io>
2018-12-14 13:41:53 -0800

    Update webpack rules to include `uuid` package in the `arccore`... ... distribution package bundle.

--- PROJECT/webpack-ignore-modules.js
+++ PROJECT/webpack-ignore-modules.js
@@ -7,19 +7,8 @@ module.exports = fs.readdirSync('node_modules').filter(function(packageName) {
     // by this filter.
     var includeInPackedOutput = false
     switch (packageName) {
-    case '.bin':
-    case 'node-uuid':
+    case 'uuid':
     case 'murmurhash-js':
-    // case 'commander':
-    // case 'graceful-readlink':
-    // case 'chalk':
-    // case 'handlebars':
-    // case 'has-ansi':
-    // case 'ansi-styles':
-    // case 'ansi-regex':
-    // case 'escape-string-regexp':
-    // case 'strip-ansi':
-    // case 'supports-color':
         includeInPackedOutput = true;
         break;
     default:
